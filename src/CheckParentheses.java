975ab05ee644e87271cd14cac396572eb97cbc63
chamathamarasinghe96 <e16022@eng.pdn.ac.lk>
2025-04-23 00:00:07 +0530

    Started implementing CheckParentheses function

---
+++ src/CheckParentheses.java
@@ -0,0 +1,11 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class CheckParentheses {
+    public boolean isValid(String s) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+
+        
+        return true;
+    }
+}
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
     public static void main(String[] args) {
-        System.out.println(new Palindrome().isPalindrome(1234321));
+        System.out.println(new CheckParentheses().isValid("{[null]}"));
     }
 }

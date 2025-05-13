fa2d1ac27e9c2fd9eabe67df63349149b6b06cfd
chamathamarasinghe96 <e16022@eng.pdn.ac.lk>
2025-05-13 19:20:39 +0530

    Implemented finding the index of the first occurrence in a string

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,8 @@
-import helpers.ArrayFunctions;
-
 class Solution {
     public static void main(String[] args) {
-        int[] nums = {3,2,2,3};
-        int val = 3;
+        String haystack = "leetcode";
+        String needle = "leeto";
 
-        System.out.println(new RemoveElements().removeElement(nums, val));
-
-        ArrayFunctions.printArray(nums);
+        System.out.println(new FindFirstNeedleInHeystack().strStr(haystack, needle));
     }
 }

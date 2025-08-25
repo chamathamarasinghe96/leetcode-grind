acdf0334713d707b41227ec45cf9ce8279588e1a
chamathamarasinghe96 <e16022@eng.pdn.ac.lk>
2025-08-25 17:47:53 +0530

    Implemented Finding First and Last Position of Element in Sorted Array using Binary Search

--- src/FirstAndLastPosition.java
+++ src/FirstAndLastPosition.java
@@ -1,7 +1,7 @@
 
 public class FirstAndLastPosition {
 
-    public static int[] findFirstAndLastPosition(int[] nums, int target) {
+    public int[] findFirstAndLastPosition(int[] nums, int target) {
         int[] positions = new int[2];
 
         boolean isArrayFound = false;
@@ -25,6 +25,55 @@ public class FirstAndLastPosition {
             }
         }
         return new int[] { -1, -1 };
+    }
+
+    public int[] findFirstAndLastPositionBinarySearch(int[] nums, int target) {
+        if (nums.length == 1) {
+            if (nums[0] == target) {
+                return new int[] { 0, 0 };
+            } else {
+                return new int[] { -1, -1 };
+            }
+        }
+        return new int[] { findFirst(nums, target), findLast(nums, target) };
+    }
+
+    private int findLast(int[] nums, int target) {
+        int first = 0;
+        int last = nums.length - 1;
+
+        while (first <= last) {
+
+            int mid = first + (last - first) / 2;
+
+            if (nums[mid] == target && (mid == (nums.length - 1) || nums[mid + 1] > target)) {
+                return mid;
+            } else if (nums[mid] > target) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int first = 0;
+        int last = nums.length - 1;
+
+        while (first <= last) {
+
+            int mid = first + (last - first) / 2;
+
+            if (nums[mid] == target && (mid == 0 || nums[mid - 1] < target)) {
+                return mid;
+            } else if (nums[mid] < target) {
+                first = mid + 1;
+            } else {
+                last = mid - 1;
+            }
+        }
+        return -1;
     }
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public static void main(String[] args) {
-        int[] arr = {};
+        int[] arr = {3, 4, 5, 8, 9};
         int target = 6;
 
-        int[] result = FirstAndLastPosition.findFirstAndLastPosition(arr, target);
+        int[] result = new FirstAndLastPosition().findFirstAndLastPositionBinarySearch(arr, target) ;
         for (int i : result) {
             System.out.println(i);
         }
